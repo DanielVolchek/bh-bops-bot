600c34dc104d420554fd9bd9b3d2ae3f802e205d
Daniel Volchek <daniel.volchek@clover.com>
2023-10-02 15:04:56 -0400

    changed parsing

--- src/slack.ts
+++ src/slack.ts
@@ -18,18 +18,13 @@ export async function slackAddRequest (req: Request, res: Response){
     return res.status(400).json({error: "Bad Formatting"})
   }
 
-    const song = parseSong(body.text)
-    if (song.error) {
-      return res.status(200).json({text: song.error})
-    }
+  const song = parseSong(body.text)
+  if (song.error) {
+    return res.status(200).json({text: song.error})
+  }
 
-  try{
-    const result = await spotifySlackRequest(req, res, song)
-    return res.status(200).json(result)
-  }
-  catch(error){
-    return res.status(400).json({error: error})
-  }
+  const result = await spotifySlackRequest(req, res, song)
+  return res.status(200).json(result)
 };
 
 export async function slackJamRequest (req: Request, res: Response) {
@@ -55,18 +50,19 @@ export async function slackHelpRequest (req: Request, res: Response) {
 
   const GITHUB_LINK = "https://github.com/danielvolchek/bh-bops-bot"
 
-  const HELP_TEXT = "/bops-add:\n>*Description*: This command allows you to add a song to the group playlist.\n>*Usage*: /bops-add <song name>-<artist name>\n>*Example*: /bops-add White Ferrari - Frank Ocean\n\n/bops-help:\n>*Description*: Provides usage instructions for all Bops commands.\n>*Usage*: Simply type /bops-help to get information about how to use each command. No additional arguments are needed.\n\n/bops-jam:\n>*Description*: Get link to group Spotify Jam, where everyone can listen together.\n>*Usage*: /bops-jam\n\n/bops-playlist:\n>*Description*: This command gives you a direct link to the group playlist.\n>*Usage*: /bops-playlist\n\nIf you have any other questions, feel free to ask! If you would like to check out how this is implemented or contribute, check out the code here: " + GITHUB_LINK
+  const HELP_TEXT = "/bops-add:\n>*Description*: This command allows you to add a song to the group playlist.\n>*Usage*: /bops-add <song name> - <artist name>\n>*Example*: /bops-add White Ferrari - Frank Ocean\n\n/bops-help:\n>*Description*: Provides usage instructions for all Bops commands.\n>*Usage*: Simply type /bops-help to get information about how to use each command. No additional arguments are needed.\n\n/bops-jam:\n>*Description*: Get link to group Spotify Jam, where everyone can listen together.\n>*Usage*: /bops-jam\n\n/bops-playlist:\n>*Description*: This command gives you a direct link to the group playlist.\n>*Usage*: /bops-playlist\n\nIf you have any other questions, feel free to ask! If you would like to check out how this is implemented or contribute, check out the code here: " + GITHUB_LINK
 
   res.status(200).send(HELP_TEXT)
   
 }
 
 const parseSong = (text: string) : Song => {
-  const parts = text.split('-').map(p => p.trim())
+
+  const parts = text.split(' - ').map(p => p.trim())
   
   if (parts.length == 2) {
     return {title: parts[0], artist: parts[1]}
   }
 
-  return {title: '', artist: '', error: 'Error: Bad Formatting, please try again'}
+  return {title: '', artist: '', error: 'Error: Bad Formatting, please ensure you are formatting as "Song - Artist" without quotes'}
 }
